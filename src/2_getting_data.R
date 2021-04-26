559dad4dc6bed7ad2f4d8a5ed423cfa42420c3df
Nick Dawe <Nick.Dawe@pilbaraports.com.au>
2021-04-27 01:01:11 +0800

    Updates to get data

--- src/2_getting_data.R
+++ src/2_getting_data.R
@@ -11,35 +11,46 @@
   # install.packages("readr")
   library(readr)
   
-  dat <- read._csv()
+  dat <- read_csv()
 
 
 
 # 3 - Download and extract data -------------------------------------------
-
-  #Create a list of web URLS to download the data
-  urls <- c("https://www.stats.govt.nz/assets/Uploads/2018-Census-totals-by-topic/Download-data/2018-census-totals-by-topic-national-highlights-csv.zip")
-
-  urls <- c("https://www.data.brisbane.qld.gov.au/data/dataset/09a0fadf-1f4b-4ebc-ada1-34a78675be85/resource/15bc533a-efdb-4c13-8779-bb853b2eccf5/download/flood-creek-study-perrin-creek-metadata.csv")
   
+  library(readr)
+
+  # Create a list of web URLS to download the data
+  urls <- c("https://www.data.brisbane.qld.gov.au/data/dataset/09a0fadf-1f4b-4ebc-ada1-34a78675be85/resource/15bc533a-efdb-4c13-8779-bb853b2eccf5/download/flood-creek-study-perrin-creek-metadata.csv",
+            "https://www.stats.govt.nz/assets/Uploads/2018-Census-totals-by-topic/Download-data/2018-census-totals-by-topic-national-highlights-csv.zip")
+  
+    
    # Download
   for(i in 1:length(urls)){
     
     download.file(url = urls[i], here::here("data", basename(urls[i])))
     
+    if(strsplit(basename(urls[i]), split="\\.")[[1]][2] == "zip"){
+      
+      unzip(here::here("data", basename(urls[i])), 
+            exdir = here::here("data"))
+      
+    }
+    
   }
   
+
   # Extract
   
-
   # Create an empty list object to extract the data into
   dat_list <- list()
   
+  files_csv <- list.files(here::here("data"), pattern = ".csv$")
+  
   # Now loop through the file names and extract to dat_list
-  for(i in  1:length(urls)){
+  for(i in  1:length(files_csv)){
     
-    dat_list[[i]] <- read_csv(here::here("data", basename(urls[i])))
-    names(dat_list)[i] <- gsub(".csv.gz", "", basename(urls[i]))
+    dat_list[[i]] <- read_csv(here::here("data", files_csv[i]))
+    names(dat_list)[i] <- gsub(".csv", "", files_csv[i])
     
   }
   
